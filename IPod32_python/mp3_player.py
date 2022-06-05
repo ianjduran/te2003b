c6cc971598141443e73f03a32edc4e3dd7fd8429
Ian <ianjduran@gmail.com>
2022-06-05 18:16:39 -0500

    project structure and rpi comm

--- IPod32_python/mp3_player.py
+++ IPod32_python/mp3_player.py
@@ -6,22 +6,29 @@ from glob import glob
 import serial
 
 
-
-
+meta_info =  {vlc.Meta.Title: None,
+            vlc.Meta.Artist: None,
+            vlc.Meta.Album: None,
+            vlc.Meta.Date: None}
 
 def set_song(instance_player, instance_vlc, song_path):
     Media = instance_vlc.media_new(song_path)
     instance_player.set_media(Media)
 
+def get_metadata(song):
+    temp = meta_info.copy()
+    for key in meta_info.keys():
+        v = song.get_meta(key)
+        temp[key] = v
+    return temp
+
 #play button function
 def on_play():
     if (player.is_playing() == 0 and player.get_media() == None): #Not playing and has no song
         title.config(text='Whoops. Something\'s wrong. Let\'s try again')
-        ser.write(b'err')
         return
     if(player.is_playing() == 0):
         player.play()
-        ser.write(b'play\r\n')
         play_btn["text"]="Pause"
         sleep(0.1)
         # get song info
@@ -33,26 +40,40 @@ def on_play():
         # set text
         title.config(text=str(current_song.get_meta(0)))
         status_label.config(text="NOW PLAYING:")
+        # set image
+        play_btn.configure(image=pause_icon)
+        play_btn.image = pause_icon
+        print("-----SERIAL-----")
+        ## TODO FIX FOR MORE INFO, JUST WORKS FOR TITLE
+        track_info = get_metadata(current_song)
+        tmp = (str(track_info[0]) + '\0').encode('ascii')
+        ser.write(tmp)
+        print("-----SERIAL-----")
     else:
-        play_btn["text"]="Play"
-        ser.write(b'pause\r\n')
-        status_label.config(text="PAUSED")
         player.pause()
+        play_btn["text"]="Play"
+        status_label.config(text="PAUSED")
+        # set image
+        play_btn.configure(image=play_icon)
+        play_btn.image = play_icon
+
+        
     show_label()
 
 #stop button function
 def on_stop():
-    ser.write(b'stop\r\n')
     player.stop()
     title.config(text="")
     status_label.config(text="STOPPED")
     play_btn["text"]="Play"
+    # set image
+    play_btn.configure(image=play_icon)
+    play_btn.image = play_icon
 
 #next button function
 def on_next():
     global Media
     global current_song
-    ser.write(b'next')
     player.stop()
     next_song=current_song+1
     # check if there is next song
@@ -65,12 +86,14 @@ def on_next():
         on_play()
     else: # there is no next song
         title.config(text="You reached the end!")
+        # set image
+        play_btn.configure(image=play_icon)
+        play_btn.image = play_icon
 
 #previous button function
 def on_previous():
     global current_song
     global Media
-    ser.write(b'previous')
     player.stop()
     next_song=current_song-1
     # Check if there is a previous song
@@ -93,13 +116,13 @@ def handle_serial_rx(input):
     # TODO Add more commands
     input = input.strip().decode()
     print(input)
-    if input == 'z':
+    if input == '*':
         on_play()
-    elif input == 'n':
-        on_next()
-    elif input =='p':
+    elif input =='0':
         on_previous()
-    elif input == 's':
+    elif input == '#':
+        on_next()
+    elif input == 'D':
         on_stop()
 
 prev_name = ""
@@ -159,17 +182,24 @@ status_label = tkinter.Label(tkinter_root, text="WELCOME!")
 title = tkinter.Label(tkinter_root, text="Let\'s start! Just hit Play!")
 show_label()
 
+# Create photoImages
+play_icon = tkinter.PhotoImage(file='./img/play_img.png')
+pause_icon = tkinter.PhotoImage(file='./img/pause_img.png')
+prev_icon = tkinter.PhotoImage(file='./img/prev_img.png')
+next_icon = tkinter.PhotoImage(file='./img/next_img.png')
+stop_icon = tkinter.PhotoImage(file='./img/stop_img.png')
+
 # Create play Button
-play_btn = tkinter.Button(tkinter_root, text ="Play", command = on_play)
+play_btn = tkinter.Button(tkinter_root, text ="Play", image=play_icon, command = on_play)
 play_btn.grid(row=2,column=0,padx=20,pady=20)
 # Create previous Button
-prev_btn = tkinter.Button(tkinter_root, text="Previous", command = on_previous)
+prev_btn = tkinter.Button(tkinter_root, text="Previous", image=prev_icon, command = on_previous)
 prev_btn.grid(row=2,column=1,padx=20,pady=20)
 # Create next Button
-next_btn = tkinter.Button(tkinter_root, text="Next", command = on_next)
+next_btn = tkinter.Button(tkinter_root, text="Next", image=next_icon, command = on_next)
 next_btn.grid(row=2,column=2,padx=20,pady=20)
 # Create stop Button
-stop_btn = tkinter.Button(tkinter_root, text ="Stop", command = on_stop)
+stop_btn = tkinter.Button(tkinter_root, text ="Stop", image=stop_icon, command = on_stop)
 stop_btn.grid(row=2,column=3,padx=20,pady=20)
 
 tkinter_root.after(200, handle_serial)
